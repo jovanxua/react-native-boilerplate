c63fc75866c5b8d821c31cfb7f963e1ae7284119
Jovan Oilixua <jga.binhimedfi@gmail.com>
2018-07-08 12:44:41 +0800

    downgraded babel-eslint from 8.2.5 to 8.2.3 due to error related to https://github.com/babel/babel-eslint/issues/530

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
+
+const App = () => (
+  <View>
+    <Text>
+      {'HI!'}
+    </Text>
+  </View>
+);
+
+export default App;
